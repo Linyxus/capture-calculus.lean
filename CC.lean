1ec47905d8425b0b5143ff175827638bbcad7434
Yichen Xu <yichen.x@outlook.com>
2024-02-14 01:32:07 +0800

    Setup entry file

---
+++ CC.lean
@@ -0,0 +1,28 @@
+-- Syntax
+import CC.CaptureSet
+import CC.Type
+import CC.Term
+
+-- Type system
+import CC.Context
+import CC.Subcapt
+import CC.Subtype
+import CC.Typed
+
+-- Operational semantics
+import CC.EvalContext
+import CC.Reduction
+
+-- Substitution
+import CC.Subcapt.Rename
+import CC.Subcapt.Subst
+import CC.Subcapt.TypeSubst
+import CC.Subtype.Rename
+import CC.Subtype.Subst
+import CC.Subtype.TypeSubst
+import CC.Typed.Rename
+import CC.Typed.Subst
+import CC.Typed.TypeSubst
+
+-- Soundness
+import CC.Reduction.Safety
